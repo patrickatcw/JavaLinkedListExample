2f099babc0769d307692f50f0e0fbe94fe606fe4
patrickatcw <pj8392@yahoo.com>
2018-03-15 16:44:37 -0400

    set up initial structure and updated gitignore file

---
+++ src/com/me/Main.java
@@ -0,0 +1,21 @@
+package com.me;
+
+//https://docs.oracle.com/javase/7/docs/api/java/util/LinkedList.html
+//https://www.developer.com/java/data/working-with-java-linked-list.html
+//https://docs.oracle.com/javase/7/docs/api/java/util/ListIterator.html
+//https://mitchellkember.com/blog/post/tortoise-and-hare/
+/*
+not very different from the other List classes, such as ArrayList. But, the point of
+difference is in how the list is maintained at the core
+*/
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+
+
+
+}
