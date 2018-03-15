02f19b780f09efd42d4b4480bab6bca1346a8fad
patrickatcw <pj8392@yahoo.com>
2018-03-15 16:52:38 -0400

    initial part of program running

--- src/com/me/Main.java
+++ src/com/me/Main.java
@@ -9,13 +9,52 @@ not very different from the other List classes, such as ArrayList. But, the poin
 difference is in how the list is maintained at the core
 */
 
+import java.util.Iterator;
+import java.util.LinkedList;
 
+//step 1 class created
 public class Main {
 
     public static void main(String[] args) {
 
+        //step 2 create linkedlist
+        LinkedList<String> placesToVisit = new LinkedList<String>();
+
+        //step 3
+        placesToVisit.add("Berea");
+        placesToVisit.add("Chagrin Falls");
+        placesToVisit.add("Lakewood");
+        placesToVisit.add("Medina");
+        placesToVisit.add("Akron");
+        placesToVisit.add("Strongsville");
+        placesToVisit.add("Shaker Heights");
+
+        //step 4 calling method to print them out
+        printList(placesToVisit);
+
     }
 
+    //step 5 creating method to print out
+    private static void printList(LinkedList<String> linkedList) {
 
+        Iterator<String> i = linkedList.iterator();  //like a for loop
+
+        while (i.hasNext()) {                         //has next checks for next entry
+
+            System.out.println("Now visiting " + i.next());     //inext moves onto next entry
+
+        }
+
+        System.out.println("-------------------------------------------");
+
+        //step 6 run program
+        //got results; Now visiting Berea
+        //etc.
+
+
+    }
 
 }
+
+
+
